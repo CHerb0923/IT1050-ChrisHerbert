afd887092dd4e06a330cdddc2978fa6fdf3fc172
Christopher Herbert <54676624+CHerb0923@users.noreply.github.com>
2019-11-07 20:16:50 -0500

    Add files via upload

---
+++ Lab2/HelloWorldWithaddedName/Lab2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Welcome to C# Programming App
+//Displays text in two lines
+using System;
+
+public class lab
+{
+
+    //Main Method begins execution of C# application
+    public static void Main( string[] args )
+    {
+        Console.WriteLine("{0}\n{1}", "Welcome to", "C# Programming!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("From Christopher W Herbert");
+        Console.WriteLine("Hello World!\tFrom Christopher W Herbert");
+    } //end Main
+} // end Class lab
---
+++ Lab2/IntegersAdd/IntegersAdd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IntegersAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum;
+
+            Console.WriteLine("Enter First Number");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            b = int.Parse(Console.ReadLine());
+
+            sum = a + b;
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+    }
+}
